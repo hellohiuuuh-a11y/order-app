ba3eba8c00afaf44719b9d64294edfb0247667cd
hellohiuuuh-a11y <hellohiuuuh@gmail.com>
2026-01-17 15:08:29 +0900

    docs 폴더에 배포 파일 다시 추가

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev
 export default defineConfig({
   // 여기에 base 옵션을 추가합니다:
   base: '/order-app/', // 실제 깃허브 저장소 이름으로 변경
   plugins: [react()],
-});
-
+})
