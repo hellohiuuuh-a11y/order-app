26e542757c7cc7cb59fb6b2ada321214ccb7fcd1
hellohiuuuh-a11y <hellohiuuuh@gmail.com>
2026-01-17 14:55:13 +0900

    docs 폴더에 배포 파일 추가 및 Pages 설정 준비_2

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev
 export default defineConfig({
+  // 여기에 base 옵션을 추가합니다:
+  base: '/order-app/', // 실제 깃허브 저장소 이름으로 변경
   plugins: [react()],
-})
+});
+
